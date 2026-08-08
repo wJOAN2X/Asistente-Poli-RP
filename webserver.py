cf1bfda40a24f8f00030fc113df860e72aebf758
wJOAN2X <quiroga.juan.2d@gmail.com>
2026-08-08 12:02:22 -0600

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -3,7 +3,6 @@ import logging
 from flask import Flask
 from threading import Thread
 
-# Silenciar los logs molestos de Flask en la consola
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
@@ -14,7 +13,6 @@ def home():
     return "Servidor Web del Bot Activo y funcionando en Render 🚀"
 
 def run():
-
     port = int(os.environ.get("PORT", 8080))
     print(f"🌐 Iniciando servidor web en el puerto {port}...")
     app.run(host='0.0.0.0', port=port)
